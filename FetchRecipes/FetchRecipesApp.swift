6b630dfbbd956475588ebdc171271f157db23079
ReggieSean <47701635+ReggieSean@users.noreply.github.com>
2024-12-29 21:32:29 +0800

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by SeanHuang on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
